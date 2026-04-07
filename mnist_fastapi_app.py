381f8e174b45ccd00ab6cfb00ef469df1c42bb07
bhartianujk <86349735+bhartianujk@users.noreply.github.com>
2026-04-07 22:17:29 +0530

    Add files via upload

---
+++ mnist_fastapi_app.py
@@ -0,0 +1,35 @@
+
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+from tensorflow import keras
+from PIL import Image
+import io
+
+app = FastAPI()
+
+# Load trained model
+model = keras.models.load_model("mnist_cnn_model.h5")
+
+def preprocess_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert('L')
+    image = image.resize((28, 28))
+    image = np.array(image) / 255.0
+    image = image.reshape(1, 28, 28, 1)
+    return image
+
+@app.get("/")
+def home():
+    return {"message": "MNIST CNN FastAPI is running!"}
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    img = preprocess_image(contents)
+
+    prediction = model.predict(img)
+    predicted_class = int(np.argmax(prediction))
+
+    return {
+        "predicted_digit": predicted_class,
+        "confidence": float(np.max(prediction))
+    }
